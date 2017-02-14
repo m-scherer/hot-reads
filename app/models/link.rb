1091c9629f32a3ed2f31c748c197dfa7badfb03e
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-13 20:03:18 -0700

    Last minute active record change

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,6 +2,6 @@ class Link < ApplicationRecord
   belongs_to :user
 
   def self.top_ten
-    select('links.url, count(links.id) as counted').group('links.url').order('counted desc').limit(10)
+    where('links.created_at > ?', Time.now - 1.day).select('links.url, count(links.id) as counted').group('links.url').order('counted desc').limit(10)
   end
 end
